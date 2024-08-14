26f583a0f679ea9f9881669a822a83f7c5e330c3
HPK-MrJJ <150632808+HPK-MrJJ@users.noreply.github.com>
2024-08-14 11:22:44 -0400

    Update Docket_Updates.py

--- Docket_Updates/Docket_Updates.py
+++ Docket_Updates/Docket_Updates.py
@@ -3,7 +3,6 @@ import asyncio
 import json
 import aiofiles
 import aiohttp
-import discord
 import pytz
 from discord.ext import tasks
 from redbot.core import commands, Config
@@ -12,13 +11,18 @@ class Docket_Updates(red_commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    def cog_unload(self):
-        self.send_daily_message.cancel()  # Stop the task if the cog is unloaded
         self.config = Config.get_conf(self, identifier=69318888, force_registration=True)
         self.config.register_guild(alerts_channel_id=None)
         self.config.register_guild(dates_by_case = {})
         self.config.register_guild(auth_token=None)
+
+    def cog_unload(self):
+        self.send_daily_message.cancel()  # Stop the task if the cog is unloaded
+        
+
+    async def fetch_url(session, url, headers=None):
+        async with session.get(url,headers=headers) as response:
+            return await response.text()
 
     @tasks.loop(time=datetime.time(hour=12, tzinfo=pytz.timezone('America/New_York')))
     async def send_daily_message(self):
@@ -35,18 +39,18 @@ class Docket_Updates(red_commands.Cog):
         else:
             print("Please set the alerts channel id.")
 
-    async def get_info():
+    async def get_info(self):
         ret = ""
         ids = []
         auth_token = await self.config.auth_token()
         if not auth_token:
             print
         headers = {
-            f"Authorization: Token {auth_token}"
+            "Authorization": f"Token {auth_token}"
         }
         async with aiofiles.open("interesting_cases.txt", mode='r') as file:
-            for line.strip() in file.readlines():
-                ids.append(line)
+            for line in file.readlines():
+                ids.append(line.strip())
         async with aiohttp.ClientSession() as session:
             all_cases = [fetch_url(session, f"https://www.courtlistener.com/api/rest/v3/dockets/{id}/",headers=headers) for id in ids]
             responses = await asyncio.gather(*tasks)
@@ -59,7 +63,7 @@ class Docket_Updates(red_commands.Cog):
                 date1 = datetime.strptime(date_last_filing, "%Y-%m-%d")
                 date2 = datetime.strptime(dates_by_case[case_id], "%Y-%m-%d")
                 if date1 > date2:
-                    ret += f"{data["case_name"]} has new docket activity!"
+                    ret += ret += f'{data["case_name"]} has new docket activity!'
             else:
                 dates_by_case[case_id] = date_last_filing
         if ret == "":
@@ -86,10 +90,6 @@ class Docket_Updates(red_commands.Cog):
         """Set token for api requests."""
         await self.config.auth_token.set(id)  # Save the token to config
         await ctx.send(f"Token set.")
-
-    async def fetch_url(session, url, headers=None):
-        async with session.get(url,headers=headers) as response:
-            return await response.text()
          
     @commands.Cog.listener()
     async def on_message(self, message):
