85143ba537ef5b4aba967c1c9883755cd504b928
HPK-MrJJ <150632808+HPK-MrJJ@users.noreply.github.com>
2024-10-17 14:46:08 -0400

    Update Docket_Updates.py

--- Docket_Updates/Docket_Updates.py
+++ Docket_Updates/Docket_Updates.py
@@ -12,9 +12,14 @@ class Docket_Updates(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=69318888, force_registration=True)
-        self.config.register_guild(alerts_channel_id=0)
-        self.config.register_guild(dates_by_case={})
-        self.config.register_guild(auth_token=0)
+        # Register guild-specific settings
+        self.config.register_guild(
+            alerts_channel_id=0,
+            dates_by_case={},
+            auth_token=""
+        )
+        # Start the loop task
+        self.send_daily_message.start()
 
     def cog_unload(self):
         self.send_daily_message.cancel()
@@ -25,23 +30,25 @@ class Docket_Updates(commands.Cog):
 
     @tasks.loop(time=datetime.time(hour=12, tzinfo=pytz.timezone('America/New_York')))
     async def send_daily_message(self):
-        channel_id = await self.config.alerts_channel_id()
-        auth_token = await self.config.auth_token()
-        channel = self.bot.get_channel(channel_id)
-        if channel:
-            if auth_token:
-                new_stuff = await self.get_info()
-                if new_stuff:
-                    await channel.send(new_stuff)
+        for guild in self.bot.guilds:  # Loop through all guilds the bot is part of
+            channel_id = await self.config.guild(guild).alerts_channel_id()
+            auth_token = await self.config.guild(guild).auth_token()
+            channel = self.bot.get_channel(channel_id)
+            
+            if channel:
+                if auth_token:
+                    new_stuff = await self.get_info(guild)
+                    if new_stuff:
+                        await channel.send(new_stuff)
+                else:
+                    print(f"Please set the token for guild: {guild.name}")
             else:
-                print("Please set the token")
-        else:
-            print("Please set the alerts channel id.")
+                print(f"Please set the alerts channel id for guild: {guild.name}.")
 
-    async def get_info(self):
+    async def get_info(self, guild):
         ret = ""
         ids = []
-        auth_token = await self.config.auth_token()
+        auth_token = await self.config.guild(guild).auth_token()
         if not auth_token:
             return None
         
@@ -55,7 +62,7 @@ class Docket_Updates(commands.Cog):
             all_cases = [self.fetch_url(session, f"https://www.courtlistener.com/api/rest/v3/dockets/{id}/", headers=headers) for id in ids]
             responses = await asyncio.gather(*all_cases)
 
-        dates_by_case = await self.config.dates_by_case()
+        dates_by_case = await self.config.guild(guild).dates_by_case()
         for response in responses:
             data = json.loads(response)
             case_id = data['id']
@@ -68,6 +75,9 @@ class Docket_Updates(commands.Cog):
             else:
                 dates_by_case[case_id] = date_last_filing
 
+        # Save updated case dates
+        await self.config.guild(guild).dates_by_case.set(dates_by_case)
+
         return ret if ret else None
 
     @send_daily_message.before_loop
@@ -77,11 +87,13 @@ class Docket_Updates(commands.Cog):
     @commands.is_owner()
     @commands.command()
     async def set_channel_id(self, ctx, id: int):
-        await self.config.alerts_channel_id.set(id)
+        """Set the alerts channel ID for the guild."""
+        await self.config.guild(ctx.guild).alerts_channel_id.set(id)
         await ctx.send("Alerts channel set.")
 
     @commands.is_owner()
     @commands.command()
     async def set_token(self, ctx, token: str):
-        await self.config.auth_token.set(token)
+        """Set the authentication token for the court listener API."""
+        await self.config.guild(ctx.guild).auth_token.set(token)
         await ctx.send("Token set.")
